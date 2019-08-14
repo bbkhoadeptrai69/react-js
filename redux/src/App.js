197801e5966a18a18fcdf09852edc97ebeaba943
bbkhoadeptrai69 <bbkhoadeptrai69@gmail.com>
2019-08-14 13:49:27 +0700

    Update App.js

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -15,19 +14,35 @@ function App() {
   var reducer1 = (state = oldState, action) => {
     switch (action.type) {
       case "CHANGE_EDIT_STATUS":
-        return {...state,editStatus:!state.editStatus}
-        break;
+        return {...state,editStatus:!state.editStatus};
+      case "ADD_NEW":
+        return {...state, num: [...state.num, action.newItem]};  
+      case "DELETE":
+        return {...state, num: state.num.filter((value, i) => i !== action.index)};  
     
       default:
-        break;
+        return state;
     }
-    return state;
   }
 
   var store1 = redux.createStore(reducer1);
+  store1.subscribe(() => console.log(store1.getState()))
+
   console.log(store1.getState());
-  store1.dispatch({type: "CHANGE_EDIT_STATUS"})
-  console.log(store1.getState());
+  store1.dispatch({
+    type: "CHANGE_EDIT_STATUS"
+  })
+
+  store1.dispatch({
+    type: "ADD_NEW",
+    newItem: 4
+  })
+
+  store1.dispatch({
+    type: "DELETE",
+    index: 0
+  })
+
   return (
     <div>
       
